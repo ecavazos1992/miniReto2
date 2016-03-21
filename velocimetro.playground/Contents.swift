6a389d6d5b06f90e080214be578f7f57c286023f
Enrique Cavazos Alonso <enriquecavazosalonso@mac-mini-de-enrique.local>
2016-03-20 20:32:47 -0600

    Push Inicial

---
+++ velocimetro.playground/Contents.swift
@@ -0,0 +1,51 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+enum Velocidades : Int{
+    
+    case Apagado = 0, VelocidadBaja = 20, VelocidadMedia = 50, VelocidadAlta = 120
+    
+    init (velocidadInicial : Velocidades){
+        self = velocidadInicial
+    }
+    
+}
+
+class Auto{
+    
+    var velocidad : Velocidades
+    
+    init (){
+        self.velocidad = Velocidades.init(velocidadInicial : Velocidades.Apagado)
+    }
+    
+    func cambioDeVelocidad() -> ( actual : Int, velocidadEnCadena : String){
+        
+        switch self.velocidad {
+        case Velocidades.Apagado:
+            self.velocidad = Velocidades.VelocidadBaja
+            return (self.velocidad.rawValue, "Velocidad Baja")
+        case Velocidades.VelocidadBaja:
+            self.velocidad = Velocidades.VelocidadMedia
+            return (self.velocidad.rawValue, "Velocidad Media")
+        case Velocidades.VelocidadMedia:
+            self.velocidad = Velocidades.VelocidadAlta
+            return (self.velocidad.rawValue, "Velocidad Alta")
+        case  Velocidades.VelocidadAlta:
+            self.velocidad = Velocidades.VelocidadMedia
+            return (self.velocidad.rawValue, "Velocidad Media")
+        }
+        
+    }
+    
+}
+
+var auto = Auto()
+
+print("hola")
+
+for i in 1...20 {
+    print("\(i). \(auto.cambioDeVelocidad())")
+}
+
